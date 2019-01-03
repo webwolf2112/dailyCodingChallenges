09a655bdfaac7ad3f972b8d45493864b11c40bd0
Vanessa Henson <vanessa.henson@getconga.com>
2019-01-03 08:50:58 -0700

    testing reduce but not currently working correctly

--- productOfArray.js
+++ productOfArray.js
@@ -1,9 +1,7 @@
 // Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-
 // For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
-
-function productOfArray(arr) {
+function productOfArrayBrute(arr) {
     let returnSum = [];
     for(var i = 0; i < arr.length; i++) {
         let product = 1;
@@ -20,8 +18,25 @@ function productOfArray(arr) {
     console.log(returnSum);
 }
 
+// function productOfArrayBrute2(arr) {
+//     let returnSum = [];
+//     for(var i = 0; i < arr.length; i++) {
+//        returnSum.push(arr.reduce((total, currentValue, currentIndex)=> {
+//             if(currentIndex !== i) {
+//                 return total * currentValue;
+//             } else {
+//                 return total;
+//             }
+//         }));
+//     }
+//     console.log(returnSum);
+// }
+
 let testArray = [1,2,3, 4,5];
 let testArray2 = [3,2,1];
 
-productOfArray(testArray);
-productOfArray(testArray2);
+// productOfArrayBrute(testArray);
+// productOfArrayBrute(testArray2);
+
+productOfArrayBrute2(testArray);
+productOfArrayBrute2(testArray2);
